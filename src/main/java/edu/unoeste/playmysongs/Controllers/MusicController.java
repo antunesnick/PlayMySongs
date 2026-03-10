4a69284011f5dfb82b972fdc0043b6b35f67a280
Nickolas <nickolasalmeida2@gmail.com>
2026-03-10 11:47:44 -0300

    feat: implements music controller

--- src/main/java/edu/unoeste/playmysongs/Controllers/MusicController.java
+++ src/main/java/edu/unoeste/playmysongs/Controllers/MusicController.java
@@ -1,9 +1,66 @@
 package edu.unoeste.playmysongs.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import edu.unoeste.playmysongs.Models.Music;
+import edu.unoeste.playmysongs.Services.MusicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
+@RequestMapping("api")
 public class MusicController {
+
+    @Autowired
+    private MusicService musicService;
+
+    @PostMapping("add-music")
+    public ResponseEntity<Object> addMusic(String title, String artist, String style, MultipartFile file) {
+        final String UPLOAD_FOLDER = "musics" + File.separator;
+        if(title == null || artist == null || style == null || file == null)
+            return ResponseEntity.badRequest().build();
+        String archiveName = title.toLowerCase().replaceAll("\\s", "") +
+                "_" + style.toLowerCase().replaceAll("\\s", "") +
+                "_" + artist.toLowerCase().replaceAll("\\s", "");
+
+        File outputFile = new File(UPLOAD_FOLDER +archiveName);
+        File uploadFolder = new File(UPLOAD_FOLDER);
+        if(!uploadFolder.exists())
+            uploadFolder.mkdir();
+        try {
+            file.transferTo(new File(uploadFolder.getAbsolutePath() + File.separator + archiveName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Music music = new Music(title, style, artist, archiveName);
+
+        musicService.insertMusic(music);
+
+
+        return ResponseEntity.ok("");
+    }
+
+
+    @GetMapping("find-music/{musicName}")
+    public ResponseEntity<Object> findMusic(@PathVariable String musicName) {
+        if(musicName.isBlank())
+            return ResponseEntity.badRequest().build();
+
+        Music music = musicService.findMusic(musicName);
+        if(music != null)
+            return ResponseEntity.ok().body(musicService.findMusic(musicName));
+
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @GetMapping("get-music-styles")
+    public ResponseEntity<Object> getStyles() {
+        return ResponseEntity.ok().body(musicService.getStyles());
+    }
 
 
 }
