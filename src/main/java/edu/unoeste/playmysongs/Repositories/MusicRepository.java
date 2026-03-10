6b97c3afd13585f9a0b70f2e4a1ad90a71f3147e
Nickolas <nickolasalmeida2@gmail.com>
2026-03-10 11:47:58 -0300

    feat: alter music repository

---
+++ src/main/java/edu/unoeste/playmysongs/Repositories/MusicRepository.java
@@ -0,0 +1,10 @@
+package edu.unoeste.playmysongs.Repositories;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class MusicRepository {
+}
