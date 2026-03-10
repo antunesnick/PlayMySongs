34e9e46cf240e1797bb734f2cc6b6cb4e5a15f05
Nickolas <nickolasalmeida2@gmail.com>
2026-03-10 11:48:13 -0300

    feat: create skeleton from music service

---
+++ src/main/java/edu/unoeste/playmysongs/Services/MusicService.java
@@ -0,0 +1,21 @@
+package edu.unoeste.playmysongs.Services;
+
+import edu.unoeste.playmysongs.Models.Music;
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MusicService {
+
+    public void insertMusic(Music music) {
+
+    }
+
+    public @Nullable Music findMusic(String musicName) {
+        return null;
+    }
+
+    public @Nullable Music getStyles() {
+        return null;
+    }
+}
