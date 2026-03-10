2082d660112e320e9be62e7170ad8f24b0bbf10a
Nickolas <nickolasalmeida2@gmail.com>
2026-03-10 10:50:04 -0300

    Initial commit

---
+++ src/test/java/edu/unoeste/playmysongs/PlayMySongsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unoeste.playmysongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayMySongsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
