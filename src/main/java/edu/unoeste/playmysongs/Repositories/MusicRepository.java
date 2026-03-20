cade6c32a0e6df8ab048585d0f11ae78b36f1f17
Nickolas <nickolasalmeida2@gmail.com>
2026-03-20 08:44:25 -0300

    feat: implements mongo music repository

--- src/main/java/edu/unoeste/playmysongs/Repositories/MusicRepository.java
+++ src/main/java/edu/unoeste/playmysongs/Repositories/MusicRepository.java
@@ -2,6 +2,7 @@ package edu.unoeste.playmysongs.Repositories;
 
 import edu.unoeste.playmysongs.Models.Music;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface MusicRepository extends MongoRepository<Music, String> {
 
     List<Music> findByTitleContainingIgnoreCase(String title);
+
+    @Query("{ '$or': [ { 'artist': { $regex: ?0, $options: 'i' } }, { 'title': { $regex: ?0, $options: 'i' } }, { 'style': { $regex: ?0, $options: 'i' } } ] }")
+    List<Music> findMusicByArtistOrTitleOrStyleContainingIgnoreCase(String key);
 
 }
